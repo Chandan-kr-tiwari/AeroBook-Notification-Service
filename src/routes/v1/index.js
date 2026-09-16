be1ae05d13a9d0d2f67053ea9738a29e752279aa
Chandan Tiwari <chandan.kr.tiwarii@gmail.com>
2026-09-16 07:52:07 +0000

    cleanup unused files

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -7,5 +7,5 @@ const { InfoController ,NotificationController} = require('../../controllers');
 const router = express.Router();
 
 router.get('/info',InfoController.infoController)
-router.post('/mails',NotificationController.Mail)
+
 module.exports = router;
